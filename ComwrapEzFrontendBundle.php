51fcabf3cdbd48d95661e0b86253c98f7dbeb034
Chunliang Deng <cdeng@comwrap.com>
2020-01-30 20:20:22 +0100

    change extension for symfony 4

--- ComwrapEzFrontendBundle.php
+++ ComwrapEzFrontendBundle.php
@@ -3,7 +3,12 @@
 namespace Comwrap\Bundle\ComwrapEzFrontendBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Comwrap\Bundle\ComwrapEzFrontendBundle\DependencyInjection\ComwrapEzFrontendExtension;
 
 class ComwrapEzFrontendBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new ComwrapEzFrontendExtension();
+    }
 }
